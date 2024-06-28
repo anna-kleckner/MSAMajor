9c3b541aa71a28cb9625c4dd53301023315c414e
anna-kleckner <annakleckner08@gmail.com>
2024-06-28 10:36:39 -0500

    Add files via upload

---
+++ examples/list_of_lists_demo.py
@@ -0,0 +1,17 @@
+def main():
+
+    list_of_lists = [[3, 7, 9],
+                     [6, 9, 2],
+                     [1, 5, 7]]
+    
+    print(list_of_lists[0][1])  
+    
+    for item in list_of_lists:
+        print(item)
+
+    for row in range(len(list_of_lists)):
+        for column in range(len(list_of_lists[row])):
+            print(list_of_lists[row][column])
+
+
+main()
